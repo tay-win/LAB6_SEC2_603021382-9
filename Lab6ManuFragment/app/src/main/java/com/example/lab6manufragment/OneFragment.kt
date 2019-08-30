37565051e4dfe00097d933d3e65801acb6b1bbc1
tay-win <53934278+tay-win@users.noreply.github.com>
2019-08-30 16:12:27 +0700

    LAB6_SEC2_603021382-9

---
+++ Lab6ManuFragment/app/src/main/java/com/example/lab6manufragment/OneFragment.kt
@@ -0,0 +1,35 @@
+package com.example.lab6manufragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view  = inflater.inflate(R.layout.fragment_one, container,false)
+            view.btnClickFragOne.setOnClickListener(){
+                var flagment: Fragment? = null
+                flagment = TwoFragment()
+                replaceFragment(flagment)
+            }
+        return view
+    }
+
+    fun  replaceFragment(someFragment: Fragment){
+        val transaction :FragmentTransaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.frameLayout, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
